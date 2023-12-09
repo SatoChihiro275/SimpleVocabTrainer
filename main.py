29aca9b389db1249d154ddf82c4559809181566f
SatoChihiro275 <s2010323@gmail.com>
2023-12-09 13:05:15 +0900

    2023/12/09 学習を表示/非表示ボタンの追加

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# 日本語フォントの設定
+# 日本語フォントの設定（matplotlibの日本語バグ抑制のため）
 mpl.rcParams['font.family'] = 'Meiryo'
 
 ########################################################
 
-# CSVファイルから単語を読み込む関数
+### CSVファイルから単語を読み込み、リストとして返す関数 ###
 def load_words(filename):
     words = []
     try:
@@ -23,14 +23,8 @@ def load_words(filename):
         print(f"ファイルの読み込み中にエラーが発生しました: {e}")
     return words
 
-# 習熟度を更新して次の単語を表示する関数
-def update_and_next(word_label, words, index, mastery_levels, level):
-    word = word_label.cget("text")
-    mastery_levels[word] = level
-    next_word(word_label, words, index, mastery_levels)
-
-# 次の単語を表示する関数
-def next_word(label, words, index, mastery_levels):
+### 指定されたラベルに次の単語を表示し、グラフを非表示にする関数 ###
+def next_word(label, words, index, mastery_levels, frame):
     # 習熟度が最大でない単語を探す
     original_index = index[0]
     while True:
@@ -47,9 +41,47 @@ def next_word(label, words, index, mastery_levels):
         if mastery_levels[current_word] < 2:  # 習得済みでない単語を選択
             label.config(text=current_word)
             break
+    
+    # グラフを非表示にする
+    for widget in frame.winfo_children():
+        widget.destroy()
+    frame.pack_forget()
 
-# 学習進捗を表示する関数
-def show_progress(mastery_levels, root):
+### 現在表示されている単語の習熟度を更新し、次の単語を表示し、グラフを更新する関数 ###
+def update_and_next(word_label, words, index, mastery_levels, frame, progress_button, level):
+    word = word_label.cget("text")
+    mastery_levels[word] = level
+    next_word(word_label, words, index, mastery_levels, frame)
+    
+    # グラフを非表示にし、ボタンのテキストを更新
+    for widget in frame.winfo_children():
+        widget.destroy()
+    frame.pack_forget()
+    progress_button.config(text="進捗を表示")
+
+### グラフの表示と非表示を切り替える関数 ###
+# 「進捗を表示」ボタンのテキストに基づいて、グラフを表示または非表示にする
+def toggle_progress(button, frame, mastery_levels):
+    if button.cget("text") == "進捗を表示":
+        show_progress(mastery_levels, frame)
+        button.config(text="進捗を非表示")
+    else:
+        # グラフの非表示処理
+        for widget in frame.winfo_children():
+            widget.destroy()
+        frame.pack_forget()
+        button.config(text="進捗を表示")
+
+### 習熟度レベルごとの単語数をグラフとして表示する関数 ###
+# グラフはTkinterウィンドウ内に描画される
+def show_progress(mastery_levels, frame):
+    # 以前のグラフをクローズ
+    plt.close()
+
+    # 既存のグラフをクリア
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     # 習熟度レベルごとの単語数を集計
     counts = [0, 0, 0]  # 0: 未学習, 1: 学習中, 2: 習得済み
     for level in mastery_levels.values():
@@ -60,17 +92,19 @@ def show_progress(mastery_levels, root):
     ax.bar(["未学習", "学習中", "習得済み"], counts, color=['red', 'yellow', 'green'])
 
     # Tkinterウィンドウにグラフを埋め込む
-    canvas = FigureCanvasTkAgg(fig, master=root)  # rootはTkinterのメインウィンドウ
+    canvas = FigureCanvasTkAgg(fig, master=frame)
     canvas.draw()
     canvas.get_tk_widget().pack()
+    frame.pack()  # グラフを表示するフレームを表示
 
 ########################################################
 
-# メイン関数
+### メイン関数 ###
+# GUIのウィンドウとウィジェットを作成し、アプリケーションを起動する
 def main():
     root = tk.Tk()
     root.title("単語帳アプリ")
-    root.geometry("400x300")  # ウィンドウのサイズを設定
+    root.geometry("600x450")  # ウィンドウのサイズを設定
 
     words = load_words("words.csv") # 単語リストの読み込み
     current_word_index = [0]  # 現在の単語のインデックスを追跡
@@ -88,19 +122,16 @@ def main():
     word_label = tk.Label(root, text=initial_text, font=("Helvetica", 16))
     word_label.pack(pady=20)
 
-    # 次の単語に進むボタン
-    # next_button = tk.Button(root, text="次の単語", command=lambda: next_word(word_label, words, current_word_index, mastery_levels))
-    # next_button.pack()
-
     # 習熟度更新ボタン
     btn_frame = tk.Frame(root)
     btn_frame.pack(pady=10)
-    tk.Button(btn_frame, text="未学習", command=lambda: update_and_next(word_label, words, current_word_index, mastery_levels, 0)).pack(side=tk.LEFT)
-    tk.Button(btn_frame, text="学習中", command=lambda: update_and_next(word_label, words, current_word_index, mastery_levels, 1)).pack(side=tk.LEFT)
-    tk.Button(btn_frame, text="習得済み", command=lambda: update_and_next(word_label, words, current_word_index, mastery_levels, 2)).pack(side=tk.LEFT)
+    tk.Button(btn_frame, text="未学習", command=lambda: update_and_next(word_label, words, current_word_index, mastery_levels, progress_frame, progress_button, 0)).pack(side=tk.LEFT)
+    tk.Button(btn_frame, text="学習中", command=lambda: update_and_next(word_label, words, current_word_index, mastery_levels, progress_frame, progress_button, 1)).pack(side=tk.LEFT)
+    tk.Button(btn_frame, text="習得済み", command=lambda: update_and_next(word_label, words, current_word_index, mastery_levels, progress_frame, progress_button, 2)).pack(side=tk.LEFT)
 
     # 進捗表示ボタン
-    progress_button = tk.Button(root, text="進捗を表示", command=lambda: show_progress(mastery_levels, root))
+    progress_frame = tk.Frame(root)
+    progress_button = tk.Button(root, text="進捗を表示", command=lambda: toggle_progress(progress_button, progress_frame, mastery_levels))
     progress_button.pack()
 
     root.mainloop()
