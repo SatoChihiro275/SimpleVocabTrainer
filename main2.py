3a979caed84c601ba78feddfeda19b2a0d20c991
SatoChihiro275 <s2010323@gmail.com>
2023-12-10 23:26:14 +0900

    2023/12/10 単語テストモードを途中まで実装

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 import csv
+import random
 import tkinter as tk
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -109,7 +110,41 @@ def start_learning_mode(root, words):
 
 # 単語テストモードを開始する関数
 def start_test_mode(root, words):
-    pass
+    # 既存のウィジェットをクリア
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    # テストモード用のフレームを作成
+    test_frame = tk.Frame(root)
+    test_frame.pack(fill=tk.BOTH, expand=True)
+
+    # 単語表示ラベルの設定
+    word_label = tk.Label(test_frame, text="", font=("Helvetica", 24))
+    word_label.pack(pady=(10, 30))
+
+    # 「答えを表示」ボタンの設定
+    show_answer_button = tk.Button(test_frame, text="答えを表示", 
+                                   command=lambda: show_answer_and_mastery_options(
+                                       test_frame, word_label, words, mastery_frame, show_answer_button))
+    show_answer_button.pack()
+
+    # 習熟度更新ボタンの設定（初期状態では非表示）
+    mastery_frame = tk.Frame(test_frame)
+    # ここではボタンをまだパックしない
+
+    for level in ["未学習", "学習中", "習得済み"]:
+        btn = tk.Button(mastery_frame, text=level, 
+                        command=lambda l=level: update_mastery_and_next_word(
+                            word_label, words, l, mastery_frame, show_answer_button))
+        btn.pack(side=tk.LEFT, padx=5)
+
+    # 「モード選択に戻る」ボタンの設定
+    back_button = tk.Button(test_frame, text="モード選択に戻る", 
+                            command=lambda: start_mode_selection(root, words))
+    back_button.pack(pady=20)
+
+    # テストを開始する
+    display_random_word(words, word_label)
 
 ########## イベントハンドラー関数 ##########
 # 特定のカテゴリの単語を表示する関数
@@ -148,6 +183,48 @@ def count_words(words, level):
         return len(words)
     level_num = category_to_num(level)
     return sum(1 for info in words.values() if info['mastery_level'] == level_num)
+
+# ランダムに単語を表示する関数
+def display_random_word(words, label):
+    # ランダムな単語を選択
+    word = random.choice(list(words.keys()))
+    label.config(text=word)
+
+# 単語の意味を表示する関数
+def show_answer(label, words):
+    word = label.cget("text")
+    if word:
+        meaning = words.get(word, {}).get('meaning', '意味が見つかりません')
+        label.config(text=f"{word} - {meaning}")
+    else:
+        label.config(text="単語が選択されていません")
+
+# 単語と意味を表示し、習熟度更新ボタンを表示する関数
+def show_answer_and_mastery_options(root, word_label, words, mastery_frame, show_answer_button):
+    word = word_label.cget("text")
+    meaning = words.get(word, {}).get('meaning', '意味が見つかりません')
+    word_label.config(text=f"{word} - {meaning}")
+    
+    # 「答えを表示」ボタンを隠す
+    show_answer_button.pack_forget()
+    
+    # 習熟度更新ボタンを表示する
+    mastery_frame.pack()
+
+# 単語の習熟度を更新して次の単語を表示する関数
+def update_mastery_and_next_word(word_label, words, mastery_level, mastery_frame, show_answer_button):
+    word = word_label.cget("text").split(' - ')[0]
+    if word in words:
+        words[word]['mastery_level'] = category_to_num(mastery_level)
+    
+    # 習熟度更新ボタンを隠す
+    mastery_frame.pack_forget()
+    
+    # 次の単語を表示する
+    display_random_word(words, word_label)
+    
+    # 「答えを表示」ボタンを再表示する
+    show_answer_button.pack()
 
 ########## アプリケーション終了時の処理 ##########
 def on_close(root, filename, words):
